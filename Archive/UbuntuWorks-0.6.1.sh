5cfb12d85b362ad202446690d23cdc75357eb09a
roguextech <jonny@roguextech.com>
2021-04-16 23:48:03 -0400

    Updated my shell scripts

---
+++ Archive/UbuntuWorks-0.6.1.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# As used on Ubuntu 20.04.2. I am sure it will work on other versions as well. If not...
+#
+# Script that does the work for me installing the software I use onto Ubuntu 20.04.2
+# Comment out software that you do not wish to install.
+#
+# UbuntuWorks™ © Jonny P. Johnston 2021
+
+	#TODO Code here to automate sign-in to Ubuntu/Google/Microsoft accounts
+	#TODO Code here to automate changing time-zone/sleep settings
+	#TODO Code here to automate checking for additional drivers
+
+# Ubuntu software update
+	sudo apt update -y
+	sudo apt upgrade -y
+	sudo apt autoremove -y
+	
+# OpenJavaJDK and Default Java JRE
+	sudo apt install -y openjdk-8-jdk-headless
+	sudo apt install -y openjdk-11-jdk-headless default-jre-headless fonts-dejavu-extra libatk-wrapper-java libatk-wrapper-java-jni openjdk-11-jre
+	sudo apt install -y openjdk-13-jdk-headless
+	sudo apt install -y openjdk-16-jdk-headless
+	sudo apt install -y default-jre
+	# Suggested Packages: openjdk-11-jdk-headless default-jre openjdk-11-demo openjdk-11-source fonts-dejavu-extra fonts-ipafont-gothic fonts-ipafont-mincho fonts-wqy-microhei | fonts-wqy-zenhei
+
+# Developer Tools
+	sudo apt install -y build-essential
+	sudo apt install -y mingw-w64 mingw-w64-tools
+	sudo apt install -y cmake
+	sudo apt install -y gfortran
+	# Suggested Packages: gfortran-multilib gfortran-doc gfortran-9-multilib gfortran-9-doc libcoarrays-dev
+	sudo apt install -y git subversion liblapack-dev libmetis-dev liblapack-doc
+	sudo apt install -y samba
+	sudo apt install -y grub-customizer
+	sudo apt install -y gnome-tweaks gnome-shell-extensions
+	sudo apt-get install -y gedit-plugins
+	sudo apt-get install -y gnome-sushi
+	# sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+	
+# LibreOffice 7 upgrade
+	sudo add-apt-repository ppa:libreoffice/libreoffice-7-0
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	
+# Qt 5
+	sudo apt install -y qtcreator qt5-default qtbase5-dev qttools5-dev qttools5-dev-tools qtwebengine5-dev qt5-doc qt5-doc-html qtbase5-doc-html qtbase5-examples
+	# Suggested Packages: clang-10-doc glibc-doc ncurses-doc libomp-10-doc libstdc++-9-doc libx11-doc libxcb-doc libxext-doc llvm-10-doc make-doc python-pygments-doc ttf-bitstream-vera default-libmysqlclient-dev firebird-dev libpq-dev libsqlite3-dev unixodbc-dev clazy cmake g++ git kate-data subversion valgrind
+  
+# Anaconda
+  # Install libraries needed by Anaconda to launch Navigator. This must be completed BEFORE installing Anaconda from the shell script.
+	sudo apt install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
+	# Install Anaconda from script on memory stick named jarvis-data
+	bash /media/roguextech/jarvis-v1-data/Software/Anaconda/Anaconda3-2020.11-Linux-x86_64.sh
+	# Update Anaconda installation
+	conda update -y anaconda
+	conda update -y spyder
+	conda update -y jupyterlab
+	conda update -y notebook
+	conda install -y spyder-notebook -c spyder-ide
+	conda install spyder=5.0.0
+	conda upgrade -y anaconda
+	anaconda-navigator
+	
+	# Anaconda uninstall: If I fuck up the install reminder to myself
+	rm -rf ~/anaconda3 ~/.condarc ~/.conda ~/.continuum
+	
+# GNU Octive
+	sudo apt install -y octave
+	sudo apt install -y octave-doc octave-info octave-htmldoc liboctave-dev
+	sudo apt install -y octave-control octave-image octave-io octave-optim octave-signal octave-statistics
+
+# Snap software installs
+	# sudo snap install -y IntelliJ-IDEA-Community
+	# sudo snap install -y vscode-insiders
+	# sudo snap install -y slack
+	# sudo snap install -y discord
+	# sudo snap install -y freecad
+	# sudo snap install -y kicad
+
+# Calibre
+	sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+	# If error use this:
+	sudo -v && wget --no-check-certificate -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+	
+# MATLAB
+	# Username: jonny@roguextech.com
+	# License ID: 8788782
+
+# Firefox Nightly
+	sudo apt remove -y firefox
+	sudo apt remove -y thunderbird
+	sudo snap install firefox --beta
+	sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa
+	sudo apt-get update
+	sudo apt-get install -y firefox-trunk
--- UbuntuWorks-0.7.0.sh
+++ UbuntuWorks-0.7.0.sh
@@ -7,39 +7,34 @@
 #
 # UbuntuWorks™ © Jonny P. Johnston 2021
 
-	#TODO Code here to sign-in to Ubuntu/Google/Microsoft accounts
-	#TODO Code here to change time-zone/sleep settings
-	#TODO Code here to check other sources for graphics drivers
+	#TODO Code here to automate sign-in to Ubuntu/Google/Microsoft accounts
+	#TODO Code here to automate changing time-zone/sleep settings
+	#TODO Code here to automate checking for additional drivers
 
-	# Ubuntu software update
+# Ubuntu software update
 	sudo apt update -y
 	sudo apt upgrade -y
 	sudo apt autoremove -y
 	
 # OpenJavaJDK and Default Java JRE
-	sudo apt install -y openjdk-8-jdk-headless
+	# sudo apt install -y openjdk-8-jdk-headless
 	sudo apt install -y openjdk-11-jdk-headless
-	sudo apt install -y openjdk-13-jdk-headless
-	sudo apt install -y openjdk-16-jdk-headless
+	# sudo apt install -y openjdk-13-jdk-headless
+	# sudo apt install -y openjdk-16-jdk-headless
 	sudo apt install -y default-jre
 
 # Developer Tools
 	sudo apt install -y build-essential
-	# Suggested Packages: debian-keyring g++-multilib g++-9-multilib gcc-9-doc gcc-multilib autoconf automake libtool flex bison gcc-doc gcc-9-multilib gcc-9-locales glibc-doc libstdc++-9-doc make-doc
-	sudo apt install -y mingw-w64 mingw-w64-tools
-	# Suggested Packages: gcc-9-locales wine wine64
+	sudo apt install -y mingw-w64
+	sudo apt install -y mingw-w64-tools
 	sudo apt install -y cmake
-	# Suggested Packages: cmake-doc ninja-build
 	sudo apt install -y gfortran
-	# Suggested Packages: gfortran-multilib gfortran-doc gfortran-9-multilib gfortran-9-doc libcoarrays-dev
-	sudo apt install -y git subversion patch wget pkg-config liblapack-dev libmetis-dev liblapack-doc
-	# Suggested Packages: git-daemon-run | git-daemon-sysvinit git-doc git-el git-email git-gui gitk gitweb git-cvs git-mediawiki git-svn db5.3-util libapache2-mod-svn subversion-tools
+	sudo apt install -y git subversion liblapack-dev libmetis-dev liblapack-doc
 	sudo apt install -y samba
-	# Suggested Packages: python-markdown-doc python-pygments-doc ttf-bitstream-vera python-pyparsing-doc bind9 bind9utils ctdb ldb-tools ntp | chrony smbldap-tools winbind heimdal-clients
-	sudo apt-get install -y gnome-tweaks
-	# Suggested Packages: chromium | chromium-browser
+	sudo apt install -y gnome-tweaks gnome-shell-extensions
 	sudo apt-get install -y gedit-plugins
 	sudo apt-get install -y gnome-sushi
+	# sudo apt install -y grub-customizer
 	# sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
 	
 # LibreOffice 7 upgrade
@@ -49,12 +44,13 @@
 	
 # Qt 5
 	sudo apt install -y qtcreator qt5-default qtbase5-dev qttools5-dev qttools5-dev-tools qtwebengine5-dev qt5-doc qt5-doc-html qtbase5-doc-html qtbase5-examples
-
+  
 # Anaconda
-  # Install libraries needed by Anaconda to launch Navigator. This must be completed BEFORE installing Anaconda from the shell script.
+  # Install libraries needed by Anaconda to launch Navigator.
+  # This must be completed BEFORE installing Anaconda from the shell script.
 	sudo apt install -y libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 	# Install Anaconda from script on memory stick named jarvis-data
-	bash Anaconda3-2020.11-Linux-x86_64.sh
+	bash /media/roguextech/jarvis-v1-data/Software/Anaconda/Anaconda3-2020.11-Linux-x86_64.sh
 	# Update Anaconda installation
 	conda update -y anaconda
 	conda update -y spyder
@@ -85,9 +81,6 @@
 	# If error use this:
 	sudo -v && wget --no-check-certificate -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
 	
-# MEGAsync
-	bash megasync-xUbuntu_20.04_amd64.deb
-
 # MATLAB
 	# Username: jonny@roguextech.com
 	# License ID: 8788782
@@ -95,6 +88,8 @@
 # Firefox Nightly
 	sudo apt remove -y firefox
 	sudo apt remove -y thunderbird
+	sudo snap install firefox --beta
 	sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa
+	sudo apt update
 	sudo apt-get update
 	sudo apt-get install -y firefox-trunk
