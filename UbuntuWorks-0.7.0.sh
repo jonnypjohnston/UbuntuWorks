be3ef509b2ca9263605b6f22ea9fd962abd6dc43
roguextech <jonny@roguextech.com>
2021-04-25 02:04:18 -0400

    Yet another update to my script

--- UbuntuWorks-0.7.0.sh
+++ UbuntuWorks-0.7.0.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# ------------------------------------------------------------------------------
-# As used on Ubuntu 20.04.2. I am sure it will work on other versions as well.
-# Script installs software I use onto Ubuntu 20.04.3
+
+# As used on Ubuntu 20.04.2. I am sure it will work on other versions as well. If not...
+#
+# Script that does the work for me installing the software I use onto Ubuntu 20.04.2
 # Comment out software that you do not wish to install.
+#
 # UbuntuWorks™ © Jonny P. Johnston 2021
-# ------------------------------------------------------------------------------
+
 	#TODO Code here to automate sign-in to Ubuntu/Google/Microsoft accounts
 	#TODO Code here to automate changing time-zone/sleep settings
 	#TODO Code here to automate checking for additional drivers
@@ -15,10 +17,10 @@
 	sudo apt autoremove -y
 	
 # OpenJavaJDK and Default Java JRE
-	sudo apt install -y openjdk-8-jdk-headless
+	# sudo apt install -y openjdk-8-jdk-headless
 	sudo apt install -y openjdk-11-jdk-headless
-	sudo apt install -y openjdk-13-jdk-headless
-	sudo apt install -y openjdk-16-jdk-headless
+	# sudo apt install -y openjdk-13-jdk-headless
+	# sudo apt install -y openjdk-16-jdk-headless
 	sudo apt install -y default-jre
 
 # Developer Tools
@@ -32,8 +34,13 @@
 	sudo apt install -y gnome-tweaks gnome-shell-extensions
 	sudo apt-get install -y gedit-plugins
 	sudo apt-get install -y gnome-sushi
-	sudo apt install -y grub-customizer
-	sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+	# sudo apt install -y grub-customizer
+	# sudo apt install libcanberra-gtk-module libcanberra-gtk3-module
+	
+# Solaar
+	sudo add-apt-repository ppa:solaar-unifying/ppa
+	sudo apt-get update
+	sudo apt-get install -y solaar
 	
 # LibreOffice 7 upgrade
 	sudo add-apt-repository ppa:libreoffice/libreoffice-7-0
@@ -67,22 +74,12 @@
 	sudo apt install -y octave-control octave-image octave-io octave-optim octave-signal octave-statistics
 
 # Snap software installs
-	sudo snap install -y IntelliJ-IDEA-Community
-	sudo snap install -y vscode-insiders
-	sudo snap install -y slack
-	sudo snap install -y discord
-	sudo snap install -y freecad
-	sudo snap install -y kicad
-
-# FreeCAD Daily Build Install
-	sudo add-apt-repository ppa:freecad-maintainers/freecad-daily
-	sudo apt-get update
-	sudo apt-get install freecad-daily
-	# Update
-	sudo apt-get update
-	sudo apt-get install freecad-daily
-	# StartUp
-	freecad-daily
+	# sudo snap install -y IntelliJ-IDEA-Community
+	# sudo snap install -y vscode-insiders
+	# sudo snap install -y slack
+	# sudo snap install -y discord
+	# sudo snap install -y freecad
+	# sudo snap install -y kicad
 
 # Calibre
 	sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
@@ -93,18 +90,6 @@
 	# Username: jonny@roguextech.com
 	# License ID: 8788782
 
-# Microsoft Edge Dev
-	## Setup
-	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-	sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-	sudo rm microsoft.gpg
-	## Install
-	sudo apt update
-	sudo apt install microsoft-edge-dev
-	## Upgrade
-	sudo apt upgrade microsoft-edge-dev
-
 # Firefox Nightly
 	sudo apt remove -y firefox
 	sudo apt remove -y thunderbird
@@ -113,28 +98,3 @@
 	sudo apt update
 	sudo apt-get update
 	sudo apt-get install -y firefox-trunk
-	sudo apt upgrade firefox-trunk
-
-# OpenFOAM
-	bash /media/roguextech/jarvis-v1-data/Software/openfoam/add-openfoam-repo.sh
-	# or
-	curl -s https://dl.openfoam.com/add-debian-repo.sh | sudo bash
-	wget -q -O - https://dl.openfoam.com/add-debian-repo.sh | sudo bash
-	deb [arch=amd64] https://dl.openfoam.com/repos/deb DIST main
-	# Versions
-	# minimalist
-	sudo apt-get install openfoam2012
-	# traditional
-	sudo apt-get install openfoam2012-dev
-	# everything
-	sudo apt-get install openfoam2012-default
-	# Manually define the location of the OpenFOAM directory as follows:
-	export WM_PROJECT_DIR=/usr/share/openfoam
-	# Uninstall
-	sudo apt-get autoremove openfoam2012-"package-name"
-	
-
-
-
-
-
